ffd623a703ffbb0d2cd0cf428a311837d383bffa
ruimalheiro <rmpmalheiro@gmail.com>
2014-03-14 21:55:46 +0000

    Removed PIL - Now using PhotoImage; Added *.gif files.

--- installer.py
+++ installer.py
@@ -1,19 +1,10 @@
-from Tkinter import Tk, Frame, BOTH, Label, Button
-
-USE_IMAGES = True
-
-try:
-	from PIL import Image, ImageTk
-except ImportError:
-	print "Please check if PIL is installed."
-	USE_IMAGES = False
-
-import tkMessageBox
+from Tkinter import Tk, Frame, BOTH, Label, Button, RIGHT, PhotoImage
 from ttk import Style
+import tkMessageBox
 import sys
 
-sys.path.insert(0, './ka-lite')
 
+sys.path.insert(0, './ka-lite')
 from kalite import version
 
 current_version = version.VERSION
@@ -34,40 +25,33 @@ class Installer(Frame):
         self.pack(fill=BOTH, expand=1)
         self.centerWindow()
 
-        #Style().configure("TButton", font='serif 10')
+        fle_logo_photo = PhotoImage(file="images/flelogo_resized.gif", width=455, height=150)
 
-        if USE_IMAGES:
-	        fle_logo = Image.open("images//flelogo.png")
-	        fle_logo_resized = fle_logo.resize((455,150), Image.ANTIALIAS)
-	        fle_logo_photo = ImageTk.PhotoImage(fle_logo_resized)
+        fle_label = Label(self, image=fle_logo_photo, width=452, height=150)
+        fle_label.image = fle_logo_photo
+        fle_label.place(x=-4, y=-4)
 
-	        fle_label = Label(self, image=fle_logo_photo, width=452, height=150)
-	        fle_label.image = fle_logo_photo
-	        fle_label.place(x=-4, y=-4)
+        kalite_logo_photo = PhotoImage(file="images/kalitelogo_resized.gif", width=255, height=65)
 
-	        kalite_logo = Image.open("images/kalitelogo.bmp")
-	        kalite_logo_resized = kalite_logo.resize((250,70), Image.ANTIALIAS)
-	        kalite_logo_photo = ImageTk.PhotoImage(kalite_logo_resized)
-
-	        kalite_label = Label(self, image=kalite_logo_photo, width=250, height=80)
-	        kalite_label.image = kalite_logo_photo
-	        kalite_label.place(x=0, y=160)
+        kalite_label = Label(self, image=kalite_logo_photo, width=250, height=80)
+        kalite_label.image = kalite_logo_photo
+        kalite_label.place(x=0, y=160)
 
         install_button = Button(self, text="Install", command=self.startInstall, width=24, height=5)
         install_button.place(x=265, y=165)
+        #install_button.pack(side=RIGHT)
 
         quit_button = Button(self, text="Quit", command=self.quit, width=24, height=5)
         quit_button.place(x=265, y=260)
+        #quit_button.pack(side=RIGHT)
 
         version_label = Label(self, text="KA Lite version: " + str(current_version), width=30, height=5)
         version_label.place(x=0, y=250)
 
-        self.pack()
-
 
     def centerWindow(self):
 
-    	width = 450
+    	width = 445
     	height = 350
 
     	screen_width = self.parent.winfo_screenwidth()
@@ -88,9 +72,6 @@ def main():
   
     root_window = Tk()
     root_window.resizable(0,0)
-
-    if USE_IMAGES:
-    	root_window.iconbitmap("images/logo48.ico")
 
     installer = Installer(root_window)
 
