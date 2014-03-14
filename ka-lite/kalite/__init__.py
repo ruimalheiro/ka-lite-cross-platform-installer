4c500390585ee78ac69c75857682f9069dbb0087
ruimalheiro <rmpmalheiro@gmail.com>
2014-03-14 18:34:10 +0000

    Removing ka-lite folder.

--- ka-lite/kalite/__init__.py
+++
@@ -1,8 +0,0 @@
-# suppress warnings here.
-try:
-    import warnings
-    warnings.simplefilter("ignore") # any other filter was ineffecual or threw an error
-except:
-    pass
-
-from version import *
