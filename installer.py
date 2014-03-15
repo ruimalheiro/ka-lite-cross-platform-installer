3b49a8f051d02793b6a094d5f6df2d7fe2528a51
ruimalheiro <rmpmalheiro@gmail.com>
2014-03-15 14:20:20 +0000

    Fixed the positioning issues.

--- installer.py
+++ installer.py
@@ -17,7 +17,7 @@ class LicenseFrame(Frame):
     """Shows the license agreement and asks the user to accept it in order to proceed.
 
     Attributes:
-        parent: A pointer to the parent container.
+        parent: A pointer to the parent frame.
         license_area: The text area that will hold the license.
         accept_var: This variable contains the integer code that corresponds to the state of the accept_button.
         accept_button: The Checkbutton used to accept the license terms.
@@ -37,7 +37,7 @@ class LicenseFrame(Frame):
     def configureLayout(self):
         """Configures the frame and the components that belong to this frame."""
         
-        self.pack(fill=BOTH, expand=1)
+        self.pack(fill=BOTH, expand=True)
 
         self.license_area = ScrolledText(self, width=4, height=4)
         self.license_area.insert(INSERT, LICENSE)
@@ -52,7 +52,7 @@ class LicenseFrame(Frame):
     def drawLayout(self):
         """Draws the frame with all the components that were previously configured."""
 
-        self.pack(fill=BOTH, expand=1)
+        self.pack(fill=BOTH, expand=True)
         self.license_area.pack(expand=True, fill=BOTH)
         self.accept_button.pack(side=LEFT)
         self.next_button.pack(side=RIGHT, padx=5, pady=5)
@@ -78,9 +78,13 @@ class WelcomeFrame(Frame):
     """Show the welcome frame where the user can start installing KA Lite or quit.
 
     Attributes:
-        parent: A pointer to the parent container.
+        parent: A pointer to the parent frame.
         fle_logo_photo: A pointer to FLE logo image.
         kalite_logo_photo A pointer to KA Lite logo image.
+        top_frame: The top frame where FLE logo is placed.
+        bottom_frame: Bottom frame.
+        bottom_left_frame: This frame is where the KA Lite logo and the version are displayed.
+        bottom_right_frame: This frame is where install button and quit button are placed.
         fle_label: A label that shows the image poited by fle_logo_photo.
         kalite_label: A label that shows the image pointed by kalite_logo_photo. 
         install_button: A button to start the installion and proceed to the welcome window.
@@ -107,28 +111,36 @@ class WelcomeFrame(Frame):
     def configureLayout(self):
         """Configures the frame and the components that belong to this frame."""
 
-        self.pack(fill=BOTH, expand=1)
+        self.pack(fill=BOTH, expand=True)
+        self.top_frame = Frame(self)
+        self.bottom_frame = Frame(self)
+        self.bottom_left_frame = Frame(self.bottom_frame)
+        self.bottom_right_frame = Frame(self.bottom_frame)
 
-        self.fle_label = Label(self, image=self.fle_logo_photo, width=452, height=150)
+        self.fle_label = Label(self.top_frame, image=self.fle_logo_photo, width=452, height=150)
         self.fle_label.image = self.fle_logo_photo
 
-        self.kalite_label = Label(self, image=self.kalite_logo_photo, width=250, height=80)
+        self.kalite_label = Label(self.bottom_left_frame, image=self.kalite_logo_photo, width=250, height=80)
         self.kalite_label.image = self.kalite_logo_photo
 
-        self.install_button = Button(self, text="Install", command=self.showLicenseFrame, width=24, height=5)
+        self.install_button = Button(self.bottom_right_frame, text="Install", command=self.showLicenseFrame, width=24, height=5)
 
-        self.quit_button = Button(self, text="Quit", command=self.quit, width=24, height=5)
+        self.quit_button = Button(self.bottom_right_frame, text="Quit", command=self.quit, width=24, height=5)
 
-        self.version_label = Label(self, text="KA Lite version: " + str(VERSION), width=30, height=5)
+        self.version_label = Label(self.bottom_left_frame, text="KA Lite version: " + str(VERSION), width=30, height=5)
 
     def drawLayout(self):
         """Draws the frame with all the components that were previously configured."""
 
-        self.fle_label.place(x=-4, y=-4)
-        self.kalite_label.place(x=0, y=160)
-        self.install_button.place(x=265, y=165)
-        self.quit_button.place(x=265, y=260)
-        self.version_label.place(x=0, y=250)
+        self.top_frame.pack(fill=BOTH, expand=True)
+        self.bottom_frame.pack(fill=BOTH, expand=True)
+        self.bottom_left_frame.pack(fill=BOTH, expand=True, side=LEFT)
+        self.bottom_right_frame.pack(fill=BOTH, expand=True, side=RIGHT)
+        self.fle_label.pack(expand=True, fill=X)
+        self.kalite_label.pack(expand=True, fill=X)
+        self.install_button.pack(side=TOP, fill=X)
+        self.quit_button.pack(side=BOTTOM, fill=X)
+        self.version_label.pack(expand=True, fill=X)
 
     def showLicenseFrame(self):
         """Changes the frame to the license frame."""
