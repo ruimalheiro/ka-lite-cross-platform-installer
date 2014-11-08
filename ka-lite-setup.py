3c6a29e67cca24924a9ca06a193764ddcc2c8289
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2014-11-08 23:16:31 +0100

    Added installation under Linux

--- ka-lite-setup.py
+++ ka-lite-setup.py
@@ -11,9 +11,23 @@ except ImportError:
         os.chdir(current_dir)
         import toga
     elif platform.system() == "Linux":
-        print("Linux!")
-        sys.exit(0)
-        pass
+	print("We have to setup toga to proceed..\nPlease wait.")
+        current_dir = os.getcwd()
+	FNULL1 = open(os.devnull, "w")
+        os.chdir(os.path.dirname(os.path.realpath(__file__)) + "//Toga//toga//")
+        p1 = subprocess.Popen(["python", os.path.dirname(os.path.realpath(__file__)) + "//setup.py", "install"], shell=False, stdout=FNULL1, stderr=subprocess.STDOUT)
+	p1.wait()
+        os.chdir(current_dir)
+	FNULL1.close()
+
+	FNULL2 = open(os.devnull, "w")
+	os.chdir(os.path.dirname(os.path.realpath(__file__)) + "//Toga//toga-gtk//")
+        p2 = subprocess.Popen(["python", os.path.dirname(os.path.realpath(__file__)) + "//setup.py", "install"], shell=False, stdout=FNULL2, stderr=subprocess.STDOUT)
+	p2.wait()
+        os.chdir(current_dir)
+	FNULL2.close()
+	print("Please restart ka-lite-setup.py to start installing KA Lite.\n\n")
+	sys.exit(0)
 
 def button_handler(widget):
         print("Hello FLE!")
